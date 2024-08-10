0c787e6c2bc56be5a197eaa35c138b0f5dd59491
Harsh1701tech <72214235+Harsh1701tech@users.noreply.github.com>
2024-08-10 16:25:42 +0530

    Create Project Scheduling.py

---
+++ Project Scheduling.py
@@ -0,0 +1,58 @@
+class T:
+    def __init__(self, n, d, deps=None):
+        self.n = n
+        self.d = d
+        self.deps = deps if deps else []
+        self.est = 0
+        self.eft = 0
+        self.lft = float('inf')
+        self.lst = float('inf')
+#Calculation for EFT AND EST
+def fwd_pass(tasks):
+    for t in tasks:
+        if t.deps:
+            t.est = max(dep.eft for dep in t.deps)
+        else:
+            t.est = 0
+        t.eft = t.est + t.d
+#Calculation for LFT AND LST
+def bwd_pass(tasks, comp_time):
+    for t in tasks:
+        if not any(t in succ.deps for succ in tasks):
+            t.lft = comp_time
+
+    for t in reversed(tasks):
+        if not any(t in succ.deps for succ in tasks):
+            t.lft = comp_time
+        else:
+            t.lft = min(succ.lst for succ in tasks if t in succ.deps)
+        t.lst = t.lft - t.d
+
+def calc_proj_times(tasks, deadline):
+    fwd_pass(tasks)
+    earliest_comp = max(t.eft for t in tasks)
+    comp_time = max(earliest_comp, deadline)
+    bwd_pass(tasks, comp_time)
+
+    return earliest_comp, comp_time
+
+def print_times(earliest, latest):
+    print(f"Earliest completion: {earliest}")
+    print(f"Latest completion: {latest}")
+
+def main():
+    taskA = T('A', 2)
+    taskB = T('B', 1, [taskA])
+    taskC = T('C', 3, [taskA])
+    taskD = T('D', 1, [taskB])
+    taskE = T('E', 2, [taskD])
+    taskF = T('F', 1, [taskC, taskE])
+
+    tasks = [taskA, taskB, taskC, taskD, taskE, taskF]
+    deadline = 10
+
+    earliest, latest = calc_proj_times(tasks, deadline)
+    print_times(earliest, latest)
+
+if __name__ == "__main__":
+    main()
